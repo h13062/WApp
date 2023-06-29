f55b1c89900a83d86d778498a75837ada77fda35
H13062 <bgh1506@gmail.com>
2023-06-29 12:55:03 -0500

    create the landing page

--- app/index.js
+++ app/index.js
@@ -1,14 +1,17 @@
 import { View, Text } from "react-native";
 import Login from "../components/loginPage/login";
+import Landing from "../components/landingPage/landing";
 const App = () => {
   return (
     <View
       style={{
         flex: 1,
-        padding: 1,
+        padding: 0,
+        margin: 0,
       }}
     >
-      <Login />
+      {/* <Login /> */}
+      <Landing />
     </View>
   );
 };
---
+++ components/landingPage/landing.jsx
@@ -0,0 +1,155 @@
+import React from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+const Landing = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../../assets/water_background.png")}
+        style={styles.background}
+      />
+      <View style={[styles.background, styles.overlay]} />
+
+      <View style={styles.logoContainer}>
+        {/* Your logo image */}
+        <Image
+          source={require("../../assets/logo-resize.png")}
+          style={styles.logo}
+        />
+        {/* Optional logo text */}
+        {/* <Text style={styles.logoText}>Logo</Text> */}
+      </View>
+
+      <View style={styles.buttonContainer}>
+        {/* Button 1 */}
+        <TouchableOpacity
+          onPress={() => {
+            // handle onPress
+          }}
+          style={styles.btn}
+        >
+          <View style={styles.btnSecondary}>
+            <MaterialCommunityIcons
+              color="#000"
+              name="email-fast-outline"
+              size={18}
+              style={{ marginRight: 12 }}
+            />
+            <Text style={styles.btnSecondaryText}>Email</Text>
+            <View style={{ width: 24 }} />
+          </View>
+        </TouchableOpacity>
+
+        {/* Button 2 */}
+        <TouchableOpacity
+          onPress={() => {
+            // handle onPress
+          }}
+          style={styles.btn}
+        >
+          <View style={styles.btnSecondary}>
+            <MaterialCommunityIcons
+              color="#000"
+              name="google"
+              size={18}
+              style={{ marginRight: 12 }}
+            />
+            <Text style={styles.btnSecondaryText}>Google</Text>
+            <View style={{ width: 24 }} />
+          </View>
+        </TouchableOpacity>
+
+        {/* Button 3 */}
+        <TouchableOpacity
+          onPress={() => {
+            // handle onPress
+          }}
+          style={styles.btn}
+        >
+          <View style={styles.btnSecondary}>
+            <MaterialCommunityIcons
+              color="#000"
+              name="facebook"
+              size={18}
+              style={{ marginRight: 12 }}
+            />
+            <Text style={styles.btnSecondaryText}>Facebook</Text>
+            <View style={{ width: 24 }} />
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  overlay: {
+    // backgroundColor: "rgba(0,0,0,0.2)",
+  },
+  logo: {
+    paddingTop: 20,
+    width: 360, // Adjust the width as needed
+    height: 80, // Adjust the height as needed
+    alignItems: "center",
+    marginBottom: 20, // Adjust the spacing as needed
+    marginTop: 90, // Add marginTop to lower the logo
+  },
+  logoContainer: {
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  logoText: {
+    fontSize: 35,
+    fontWeight: "bold",
+    color: "white",
+    marginTop: 8,
+  },
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    marginBottom: 63,
+    width: "100%",
+    alignItems: "center",
+  },
+  btn: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    backgroundColor: "white",
+    borderColor: "#000",
+    marginBottom: 12,
+    width: "90%",
+  },
+  btnSecondary: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btnSecondaryText: {
+    fontSize: 18,
+    lineHeight: 26,
+    fontWeight: "600",
+    color: "#000",
+    marginRight: 12,
+    textAlign: "center",
+  },
+});
+
+export default Landing;
