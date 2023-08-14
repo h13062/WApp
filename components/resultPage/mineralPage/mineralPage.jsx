c55aee3a55b88c48d6f18bc48616557546ea1619
H13062 <bgh1506@gmail.com>
2023-08-14 16:45:24 -0500

    add mineral array in mineralPage.jsx

---
+++ components/resultPage/mineralPage/mineralPage.jsx
@@ -0,0 +1,19 @@
+const mineralNames = [
+  "Ca",
+  "Cr",
+  "Cu",
+  "F",
+  "Iodine",
+  "Fe",
+  "Mg",
+  "Mn",
+  "Mo",
+  "P",
+  "Se",
+  "Zn",
+  "K",
+  "Na",
+  "Cl",
+];
+
+export { mineralNames };
